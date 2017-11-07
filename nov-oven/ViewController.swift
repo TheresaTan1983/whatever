88267ee8a732f99337b0871b2d0aa8ab2084c6a2
Theresa Tan Sze Ling <theresa@Theresas-MacBook-Air.local>
2017-11-07 11:15:34 +0800

    Initial Commit

---
+++ nov-oven/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nov-oven
+//
+//  Created by Theresa Tan Sze Ling on 07/11/2017.
+//  Copyright © 2017 Theresa Tan Sze Ling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
